230c3428a3f4d0f82dd0ec44427ce4efc2361aab
luminous44 <aklemasultanakhi@gmail.com>
2025-11-07 18:49:11 +0600

    running server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/luminous44/CRUD-api
 
 go 1.25.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func health(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Ok")
+}
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/healthCheck",health)
+	// r.HandleFunc("/recipes",getAllRecipes)
+	// r.HandleFunc("/recipe",getRecipe)
+	// r.HandleFunc("/delete",deleteRecipe)
+	// r.HandleFunc("/create",createRecipe)
+	// r.HandleFunc("/update",updateRecipe)
+
+	fmt.Println("server running on 8080")
+    
+	if err := http.ListenAndServe(":8080",r); err != nil{
+        fmt.Println("Failed to start server",err)        
+		log.Fatal(err)
+	}
+
 }
