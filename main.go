2bf68201d17c32ef26b3c6de9262aba1b10584ad
luminous44 <aklemasultanakhi@gmail.com>
2025-11-07 19:19:33 +0600

    get all item

--- main.go
+++ main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-
 	"github.com/gorilla/mux"
 )
+var recipesList []Recipe
+type Recipe struct{
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Cooker *Cooker `json:"cooker"`
+}
+type Cooker struct{
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
 
 func health(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Ok")
+	fmt.Fprintln(w,"Ok")
+}
+
+func getAllRecipes(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	if err := json.NewEncoder(w).Encode(recipesList); err != nil{
+		http.Error(w,"Failed to patch data",http.StatusNotFound)
+	}
+
 }
 func main() {
-
+    recipesList = append(recipesList, Recipe{ID: 1,Name: "Biriyani",Type:"Lunch",Cooker: &Cooker{Name: "Hina",Age: 30}})
+	recipesList = append(recipesList, Recipe{ID: 2,Name: "Polaw",Type:"Dinner",Cooker: &Cooker{Name: "Zoe",Age: 20}})
 	r := mux.NewRouter()
 	r.HandleFunc("/healthCheck",health)
-	// r.HandleFunc("/recipes",getAllRecipes)
-	// r.HandleFunc("/recipe",getRecipe)
+	r.HandleFunc("/recipes",getAllRecipes).Methods("GET")
+	 //r.HandleFunc("/recipe{id}",getRecipeById)
 	// r.HandleFunc("/delete",deleteRecipe)
 	// r.HandleFunc("/create",createRecipe)
 	// r.HandleFunc("/update",updateRecipe)
